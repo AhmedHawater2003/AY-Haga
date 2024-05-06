223f9992f2fc4eeb73aba8def23c7dcbb9ed8b17
Ahmed Hawater <hawatercoding@gmail.com>
2024-05-06 16:45:54 +0300

    initialize essential components

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -1,124 +1,36 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
-
+import SearchBar from "./SearchBar";
+import DonationsGrid from "./DonationsGrid";
 
 const HowToUse = () => {
-    const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-    return (
-        <div>
-            <Navbar isLanding={false} sideBarFlag={isSideBarOpen}
-                showSideBar={() => setIsSideBarOpen(!isSideBarOpen)} />
-            <div
-                style={{
-                    margin: `20px 20px 20px ${isSideBarOpen ? "270px" : "20px"}`,
-                    transition: "200ms",
-                }}
-            >
-                <div className="accordion" id="accordionExample">
-                    <div className="accordion-item">
-                        <div className="row p-3 align-items-center ">
-                            <span className="col-8 fs-5 ">test</span>
-                            <div className="col-4">
-                                <div className="row justify-content-end ">
-                                    <button
-                                        className="col-2 btn btn-info mx-2 col"
-                                        type="button"
-                                        data-bs-toggle="collapse"
-                                        data-bs-target="#collapseOne"
-                                        aria-controls="collapseOne"
-                                    ></button>
-                                    <button className="col-2 btn btn-primary mx-2">#</button>
-                                    <button className="col-2 btn btn-danger mx-2">#</button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            id="collapseOne"
-                            className="accordion-collapse collapse show"
-                            aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample"
-                        >
-                            <div className="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is
-                                shown by default, until the collapse plugin adds the appropriate
-                                classes that we use to style each element. These classes control
-                                the overall appearance, as well as the showing and hiding via
-                                CSS transitions. You can modify any of this with custom CSS or
-                                overriding our default variables. It's also worth noting that
-                                just about any HTML can go within the{" "}
-                                <code>.accordion-body</code>, though the transition does limit
-                                overflow.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingTwo">
-                            <button
-                                className="accordion-button collapsed"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#collapseTwo"
-                                aria-expanded="false"
-                                aria-controls="collapseTwo"
-                            >
-                                Accordion Item #2
-                            </button>
-                        </h2>
-                        <div
-                            id="collapseTwo"
-                            className="accordion-collapse collapse"
-                            aria-labelledby="headingTwo"
-                            data-bs-parent="#accordionExample"
-                        >
-                            <div className="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is
-                                hidden by default, until the collapse plugin adds the
-                                appropriate classes that we use to style each element. These
-                                classes control the overall appearance, as well as the showing
-                                and hiding via CSS transitions. You can modify any of this with
-                                custom CSS or overriding our default variables. It's also worth
-                                noting that just about any HTML can go within the{" "}
-                                <code>.accordion-body</code>, though the transition does limit
-                                overflow.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingThree">
-                            <button
-                                className="accordion-button collapsed"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#collapseThree"
-                                aria-expanded="false"
-                                aria-controls="collapseThree"
-                            >
-                                Accordion Item #3
-                            </button>
-                        </h2>
-                        <div
-                            id="collapseThree"
-                            className="accordion-collapse collapse"
-                            aria-labelledby="headingThree"
-                            data-bs-parent="#accordionExample"
-                        >
-                            <div className="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is
-                                hidden by default, until the collapse plugin adds the
-                                appropriate classes that we use to style each element. These
-                                classes control the overall appearance, as well as the showing
-                                and hiding via CSS transitions. You can modify any of this with
-                                custom CSS or overriding our default variables. It's also worth
-                                noting that just about any HTML can go within the{" "}
-                                <code>.accordion-body</code>, though the transition does limit
-                                overflow.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* <Switch>
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  return (
+    <div>
+      <Navbar
+        isLanding={false}
+        sideBarFlag={isSideBarOpen}
+        showSideBar={() => setIsSideBarOpen(!isSideBarOpen)}
+      />
+      <div
+        className="content"
+        style={{
+          margin: `20px 20px 20px ${isSideBarOpen ? "270px" : "20px"}`,
+          transition: "200ms",
+        }}
+      >
+        <div className="d-flex justify-content-between bg-primary p-3 mb-3">
+          <h1>Category Name</h1>
+          <SearchBar
+            formStyling={{ width: "50%", height: "50px" }}
+            inputStyle={{ width: "50%" }}
+          />
+        </div>
+        <DonationsGrid />
+      </div>
+      <div>
+        {/* <Switch>
             <Route path="/overview" exact component={Overview} />
             <Route path="/reports" exact component={Reports} />
             <Route path="/reports/reports1" exact component={ReportsOne} />
@@ -126,8 +38,8 @@ const HowToUse = () => {
             <Route path="/reports/reports3" exact component={ReportsThree} />
             <Route path="/team" exact component={Team} />
             </Switch> */}
-            </div>
-        </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default HowToUse;
