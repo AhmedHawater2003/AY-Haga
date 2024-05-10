c797a0ec0bb1af2d873421aaf68e64ba62fd5f75
Abdullah Mahmoud <abdullahmahmoud289@gmail.com>
2024-05-10 17:03:08 +0300

    Finish styling login and register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,33 +1,49 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
 import DonorRegForm from "./DonorRegForm";
 import OrganizationsRegForm from "./OrganizationsRegForm";
-
+import { Card } from "react-bootstrap";
+import donor_icon from "../assets/donor-icon.png";
+import org_icon from "../assets/org-icon.png";
+import { SlArrowLeftCircle } from "react-icons/sl";
 
 
 const Register = () => {
-    const [show, setShow] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
-  
-    const handleClose = () => {setShow(false);setShowAlert(false);}
-    const handleShow = () => setShow(true);
-  
-    const handleAlertClose = () => setShowAlert(false);
-    const handleAlertShow = () => setShowAlert(true);
-  
+
+
     const isSuccess = false;
-  
+
     return (
-      
-        <>
-            <OrganizationsRegForm/>
-                    
-        </>
-  
-        
+        <div className="sections" style={{backgroundColor: "#1b5e30"}}>
+            <div className="card-wrapper" style={{ height: "100vh", justifyContent: "center", alignItems: "center"}}>
+                <div className="card-col" style={{ height: "50vh", backgroundColor: "#fffff9", borderRadius: "10px" }}>
+                    <a className='login-link' href="/">
+                        <p style={{textDecoration:"underline",paddingLeft:"10px",paddingTop:"10px"}}><SlArrowLeftCircle />Back To Home</p>
+                    </a>
+                    <div className="card-container" >
+                        <Card className="card" style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <img src={donor_icon} alt="donor icon" style={{ width: '50px', height: '50px' }} />
+                                <Card.Title>Donor</Card.Title>
+                                <Card.Text>
+                                    Join us in making a difference in the world by responding to the needs of others.
+                                </Card.Text>
+                                <DonorRegForm />
+                            </Card.Body>
+                        </Card>
+                        <Card className="card" style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <img src={org_icon} alt="org icon" style={{ width: '50px', height: '50px' }} />
+                                <Card.Title>Organization</Card.Title>
+                                <Card.Text>
+                                    Join us to change the way people donate to your organization.
+                                </Card.Text>
+                                <OrganizationsRegForm />
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
