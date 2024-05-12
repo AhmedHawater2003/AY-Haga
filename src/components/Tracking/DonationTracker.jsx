8ddbb3775475c697442c732c1b6ed05cc1655099
Ahmed Hawater <hawatercoding@gmail.com>
2024-05-12 03:09:31 +0300

    add donation tracking component

---
+++ src/components/Tracking/DonationTracker.jsx
@@ -0,0 +1,75 @@
+import Accordion from "react-bootstrap/Accordion";
+import "./trackerStyle.css";
+import Stepper from "./Stepper";
+import { Badge } from "@mantine/core";
+import TrackingDetials from "./TrackingDetails";
+
+const tempData = [
+  {
+    "Estimated Delivery Time": "29 May 2024",
+    "Traking Number": "BD0459652",
+    "Deliverd By": "--",
+    Contact: "--",
+  },
+
+  {
+    "Estimated Delivery Time": "2 Jun 2024",
+    "Traking Number": "BD0259752",
+    "Deliverd By": "Omar Ali",
+    Contact: "+201873456789",
+  },
+
+  {
+    "Estimated Delivery Time": "2 April 2024",
+    "Traking Number": "BD03359701",
+    "Deliverd By": "Omar Ali",
+    Contact: "+201873456789",
+    "Actual Delivery Time": "3 April 2024",
+    "Orgnization FeedBack": `5 / 5`,
+  },
+];
+
+function DonationTracker() {
+  return (
+    <Accordion className="track">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header className="track-accordion-header">
+          <span>Summer clothes for Orphans</span>
+          <Badge variant="light" color="green" size="xl" radius="lg">
+            Delivery Matching
+          </Badge>
+        </Accordion.Header>
+        <Accordion.Body>
+          <Stepper active={1} second={true} />
+          <TrackingDetials donationPrimaryDetails={tempData[0]} />
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>
+          <span>Arsenic Trioxide blood cancer drug</span>
+          <Badge variant="light" color="green" size="xl" radius="lg">
+            Waiting Delivery
+          </Badge>
+        </Accordion.Header>
+        <Accordion.Body>
+          <Stepper active={2} third={true} />
+          <TrackingDetials donationPrimaryDetails={tempData[1]} />
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>
+          <span>Physics books for secondary grade</span>
+          <Badge variant="light" color="green" size="xl" radius="lg">
+            Donation Recieved
+          </Badge>
+        </Accordion.Header>
+        <Accordion.Body>
+          <Stepper active={4} />
+          <TrackingDetials donationPrimaryDetails={tempData[2]} />
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default DonationTracker;
--- src/components/Tracking/Stepper.jsx
+++ src/components/Tracking/Stepper.jsx
@@ -1,12 +1,28 @@
 import { Stepper } from "@mantine/core";
 
-function Demo() {
+function Demo({ first, second, third, fourth, active }) {
   return (
-    <Stepper color="green" active={1} size="xl">
-      <Stepper.Step label="Step 1" description="Create an account" />
-      <Stepper.Step label="Step 2" description="Verify email" loading />
-      <Stepper.Step label="Step 3" description="Get full access" />
-      <Stepper.Step label="Step 4" description="Get full access" />
+    <Stepper color="green" active={active} size="lg" className="p-4">
+      <Stepper.Step
+        label="Step 1"
+        description="Submitting Donation"
+        loading={first}
+      />
+      <Stepper.Step
+        label="Step 2"
+        description="Delivery Matching"
+        loading={second}
+      />
+      <Stepper.Step
+        label="Step 3"
+        description="Waiting Delivery"
+        loading={third}
+      />
+      <Stepper.Step
+        label="Step 3"
+        description="Donation Recieved"
+        loading={fourth}
+      />
     </Stepper>
   );
 }
