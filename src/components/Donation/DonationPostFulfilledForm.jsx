b90bd95e6e6303cbdc272f54c1d8eb78b0c96fcd
Talaat-jr <abdelrahmanmt96@gmail.com>
2024-05-12 22:55:48 +0300

    edit fullfil

--- src/components/Donation/DonationPostFulfilledForm.jsx
+++ src/components/Donation/DonationPostFulfilledForm.jsx
@@ -1,8 +1,23 @@
 import { Button } from "@mantine/core";
+import { useState } from "react";
+import DeleteConfirmationModal from "../DeleteConfirmationModal";
+
 const DonationPostFulfilledForm = ({item , deletePost}) => {
+  const back1 = ()=>{
+    setShow(true);
+  }
+  const hide = ()=>{
+    setShow(false);
+  }
   return (
     <div>
-      <Button color ="red" onClick ={() => deletePost(item)}>Delete Post</Button>
+      <DeleteConfirmationModal
+          item={item}
+          deletePost={deletePost}
+          back={back1}
+          hide={hide} 
+        />
+      {/* <Button color ="red" onClick ={() => deletePost(item)}>Delete Post</Button> */}
     </div>
   )
 }
