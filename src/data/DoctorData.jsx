ee88f7472a4aaf7319be21987160721430acd416
Ahmed Hawater <hawatercoding@gmail.com>
2024-05-12 04:53:28 +0300

    Fix glitches in donation cards when moving between sections.
    - was done by giving each donation card in the data JSON a unique ID

--- src/data/DoctorData.jsx
+++ src/data/DoctorData.jsx
@@ -1,130 +1,131 @@
 export const DoctorData = [
-    {
-      id: 1,
-      title: "Medical Opportunity 1",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Gender' : 'Gender',
-       'Weight' : 'Weight',
-       'Address' : 'Address',
-       'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  {
+    id: 20,
+    title: "Medical Opportunity 1",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Gender: "Gender",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 2,
-      title: "Medical Opportunity 2",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 21,
+    title: "Medical Opportunity 2",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 3,
-      title: "Medical Opportunity 3",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 23,
+    title: "Medical Opportunity 3",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 4,
-      title: "Medical Opportunity 4",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 24,
+    title: "Medical Opportunity 4",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 5,
-      title: "Medical Opportunity 5",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 25,
+    title: "Medical Opportunity 5",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 6,
-      title: "Medical Opportunity 6",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 26,
+    title: "Medical Opportunity 6",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 7,
-      title: "Medical Opportunity 7",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 27,
+    title: "Medical Opportunity 7",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
-    {
-      id: 8,
-      title: "Medical Opportunity 8",
-      summary: "Summary of the case",
-      primaryDetails: {
-       'Patient Name': 'Patient Name',
-       'Age': 'Age',
-       'Weight' : 'Weight',
-        'Address' : 'Address',
-        'Medical Speciality' : 'Medical Speciality',
-      },
-      organization: "World Health Organization",
-      additionalDetails: "A lot of Stuff to be described here",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=", 
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+  {
+    id: 28,
+    title: "Medical Opportunity 8",
+    summary: "Summary of the case",
+    primaryDetails: {
+      "Patient Name": "Patient Name",
+      Age: "Age",
+      Weight: "Weight",
+      Address: "Address",
+      "Medical Speciality": "Medical Speciality",
     },
+    organization: "World Health Organization",
+    additionalDetails: "A lot of Stuff to be described here",
+    image:
+      "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  },
+];
--- src/data/TeachingData.jsx
+++ src/data/TeachingData.jsx
@@ -1,109 +1,96 @@
 export const TeachingData = [
-    {
-      id: 1,
-      title: "Math Teacher for Orphans in Giza",
-      summary: "Volunteer to teach mathematics to orphaned children in Giza.",
-      primaryDetails: {
-        'Address': "Giza, Egypt",
-        'Number of students' : "20",
-        'Subjects' : "Mathematics",
-      },
-      organization: "Hope Haven Orphanage",
-      additionalDetails: "Help provide specialized education and support to orphaned children",
-      image:
-      "https://copticorphans.org/wp-content/uploads/2021/08/Home-Page-Ask-Image-534x534-1.jpg"
-              },
-    {
-      id: 2,
-      title: "Science Teacher at Cancer Hospital (57357)",
-      summary: "Join the education team at 57357 Hospital to teach science to young patients",
-      primaryDetails: {
-        'Address': "Cairo, Egypt",
-        'Number of students' : "15",
-        'Subjects' : "Science",
-      },
-      organization: "57357 Hospital",
-      additionalDetails: "Support children battling cancer by improving their science skills",
-      image:
-      "https://www.57357.org/app/uploads/2023/05/10-4-e1684852386871.jpg"    },
-    {
-      id: 3,
-      title: "English Instructor for Rural Schools in Aswan",
-      summary: "Teach English language skills to students in rural schools along the Nile in Aswan",
-      primaryDetails: {
-        'Address': "Aswan,Egypt",
-        'Number of students' : "20",
-        'Subjects' : "English",
-      },
-      organization: "Nile Education Initiative",
-      additionalDetails: "Empower students with English proficiency to broaden their opportunities",
-      image:
-      "https://tse3.mm.bing.net/th?id=OIP.Y73VJZxcTKXz3M8c_shsIwHaFj&pid=Api&P=0&h=220"    },
-    {
-      id: 4,
-      title: "STEM Educator for After-School Program in Cairo",
-      summary: "Lead engaging STEM (Science, Technology, Engineering, Mathematics) activities for students in an after-school program in Cairo",
-      primaryDetails: {
-        'Address': "Caio, Egypt",
-        'Number of students' : "30",
-        'Subjects' : "STEM",
-      },
-      organization: "Cairo STEM Academy",
-      additionalDetails: "Inspire the next generation of innovators through hands-on STEM learning experiences.",
-      image:
-      "https://tse1.mm.bing.net/th?id=OIP.rjsmL7SVvn4m8uFfiKwRVQHaEJ&pid=Api&P=0&h=220"    },
-    {
-      id: 5,
-      title: "Art Teacher for Community Center in Alexandria",
-      summary: "Develop and implement art curriculum for children, fostering creativity and self-expression",
-      primaryDetails: {
-        'Address': "Alexandria, Egypt",
-        'Number of students' : "15",
-        'Subjects' : "Art",
-      },
-      organization: "Alexandria Community Center",
-      additionalDetails: "Harness the power of art to nurture imagination and cultural appreciation",
-      image:
-      "https://tse3.mm.bing.net/th?id=OIP.xSm7AOvZeTXEqOEZZLQwGwHaE8&pid=Api&P=0&h=220"    },
-    {
-      id: 6,
-      title: "Arabic Tutor for Refugee Center in Cairo",
-      summary: "Support newly arrived refugees in Cairo by teaching them Arabic language skills",
-      primaryDetails: {
-        'Address': "Cairo, Egypt",
-        'Number of students' : "35",
-        'Subjects' : "Arabic",
-      },
-      organization: "Egyptian Red Crescent Society",
-      additionalDetails: "Help refugees integrate into Egyptian society by equipping them with essential communication skills",
-      image:
-"https://tse2.mm.bing.net/th?id=OIP.0U8tUFki3r37wG0wsEuY5wHaE7&pid=Api&P=0&h=220"    },
-    {
-      id: 7,
-      title: "Teaching Opportunity 7",
-      summary: "Summary of the case",
-      primaryDetails: {
-        'Address': "Address",
-        'Number of students' : "Number of students",
-        'Subjects' : "Subjects",
-      },
-      organization: "World Health Organization",
-      additionalDetails: "Nothing",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+  {
+    id: 10,
+    title: "Math Teacher for Orphans in Giza",
+    summary: "Volunteer to teach mathematics to orphaned children in Giza.",
+    primaryDetails: {
+      Address: "Giza, Egypt",
+      "Number of students": "20",
+      Subjects: "Mathematics",
     },
-    {
-      id: 8,
-      title: "Teaching Opportunity 8",
-      summary: "Summary of the case",
-      primaryDetails: {
-        'Address': "Address",
-        'Number of students' : "Number of students",
-        'Subjects' : "Subjects",
-      },
-      organization: "World Health Organization",
-      additionalDetails: "Nothing",
-      image:
-        "https://media.istockphoto.com/id/1318617341/photo/low-angle-view-group-of-volunteers-busy-working-by-arranging-vegetables-and-clothes-on.jpg?s=612x612&w=0&k=20&c=CJsDJ1nIMKr5NPBQufc5eyjsbSV_3XhqgcQusavGXIk=",
+    organization: "Hope Haven Orphanage",
+    additionalDetails:
+      "Help provide specialized education and support to orphaned children",
+    image:
+      "https://copticorphans.org/wp-content/uploads/2021/08/Home-Page-Ask-Image-534x534-1.jpg",
+  },
+  {
+    id: 11,
+    title: "Science Teacher at Cancer Hospital (57357)",
+    summary:
+      "Join the education team at 57357 Hospital to teach science to young patients",
+    primaryDetails: {
+      Address: "Cairo, Egypt",
+      "Number of students": "15",
+      Subjects: "Science",
     },
-  ]
+    organization: "57357 Hospital",
+    additionalDetails:
+      "Support children battling cancer by improving their science skills",
+    image: "https://www.57357.org/app/uploads/2023/05/10-4-e1684852386871.jpg",
+  },
+  {
+    id: 12,
+    title: "English Instructor for Rural Schools in Aswan",
+    summary:
+      "Teach English language skills to students in rural schools along the Nile in Aswan",
+    primaryDetails: {
+      Address: "Aswan,Egypt",
+      "Number of students": "20",
+      Subjects: "English",
+    },
+    organization: "Nile Education Initiative",
+    additionalDetails:
+      "Empower students with English proficiency to broaden their opportunities",
+    image:
+      "https://tse3.mm.bing.net/th?id=OIP.Y73VJZxcTKXz3M8c_shsIwHaFj&pid=Api&P=0&h=220",
+  },
+  {
+    id: 13,
+    title: "STEM Educator for After-School Program in Cairo",
+    summary:
+      "Lead engaging STEM (Science, Technology, Engineering, Mathematics) activities for students in an after-school program in Cairo",
+    primaryDetails: {
+      Address: "Caio, Egypt",
+      "Number of students": "30",
+      Subjects: "STEM",
+    },
+    organization: "Cairo STEM Academy",
+    additionalDetails:
+      "Inspire the next generation of innovators through hands-on STEM learning experiences.",
+    image:
+      "https://tse1.mm.bing.net/th?id=OIP.rjsmL7SVvn4m8uFfiKwRVQHaEJ&pid=Api&P=0&h=220",
+  },
+  {
+    id: 14,
+    title: "Art Teacher for Community Center in Alexandria",
+    summary:
+      "Develop and implement art curriculum for children, fostering creativity and self-expression",
+    primaryDetails: {
+      Address: "Alexandria, Egypt",
+      "Number of students": "15",
+      Subjects: "Art",
+    },
+    organization: "Alexandria Community Center",
+    additionalDetails:
+      "Harness the power of art to nurture imagination and cultural appreciation",
+    image:
+      "https://tse3.mm.bing.net/th?id=OIP.xSm7AOvZeTXEqOEZZLQwGwHaE8&pid=Api&P=0&h=220",
+  },
+  {
+    id: 15,
+    title: "Arabic Tutor for Refugee Center in Cairo",
+    summary:
+      "Support newly arrived refugees in Cairo by teaching them Arabic language skills",
+    primaryDetails: {
+      Address: "Cairo, Egypt",
+      "Number of students": "35",
+      Subjects: "Arabic",
+    },
+    organization: "Egyptian Red Crescent Society",
+    additionalDetails:
+      "Help refugees integrate into Egyptian society by equipping them with essential communication skills",
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.0U8tUFki3r37wG0wsEuY5wHaE7&pid=Api&P=0&h=220",
+  },
+];
