5dfeb27ea187cb56c84be1901e48a1ffb997c0ac
Ahmed Hawater <hawatercoding@gmail.com>
2024-05-12 13:02:38 +0300

    Add search results view with back arrow to donor page

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -1,23 +1,42 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
 import DonationsGrid from "./Donation/DonationsGrid";
-import FilterBar from "./FiltrationBar/FilterBar";
+import styled from "styled-components";
+import { IconArrowLeft } from "@tabler/icons-react";
 import { DonorSideBarData, allData } from "../data/DonorSideBarData";
 import { DonationCategoriesData } from "../data/DonationCategoriesData";
 
+const HomePage = (
+  <DonationsGrid
+    data={allData}
+    isDoner={true}
+    isFullfilled={false}
+    isPending={false}
+    isWaiting={false}
+  />
+);
 const HowToUse = () => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-  const [content, setContent] = useState(
-    <DonationsGrid
-      data={allData}
-      isDoner={true}
-      isFullfilled={false}
-      isPending={false}
-      isWaiting={false}
-    />
-  );
+  const [content, setContent] = useState(HomePage);
   const sideBarFunction = (value) => {
-    setContent(value.content);
+    setContent((prev) => value.content);
+  };
+
+  const searchBarFunction = (category) => {
+    setContent((prev) => (
+      <>
+        <div className="d-flex py-2 mb-1 align-items-center ">
+          <IconArrowLeft
+            color="green"
+            size={42}
+            onClick={() => setContent(prev)}
+            cursor={"pointer"}
+          />
+          <h3 style={{ margin: "auto" }}>Search Results</h3>
+        </div>
+        {category.content}
+      </>
+    ));
   };
 
   return (
@@ -26,7 +45,7 @@ const HowToUse = () => {
         sideBarFlag={true}
         searchBarFlag={true}
         searchBarData={DonationCategoriesData}
-        searchBarFunction={(item) => setContent(item.content)}
+        searchBarFunction={searchBarFunction}
         isSideBarOpen={isSideBarOpen}
         showSideBar={() => setIsSideBarOpen(!isSideBarOpen)}
         sideBarData={DonorSideBarData}
